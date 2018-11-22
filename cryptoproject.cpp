192ed92baf708e0d4671e9bad59986c365ae94f6
Andrew Chin <andrew_chin@tamu.edu>
2018-11-22 14:31:25 -0600

    initial commit

---
+++ cryptoproject.cpp
@@ -0,0 +1,28 @@
+/*
+    Author: Andrew Chin
+    UIN: 425006084
+    MATH 470-502
+    Project: RSA Public Key El Gamal Digital Signature
+*/
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+
+
+/*--------------------------------------------------------------------------*/
+/* MAIN FUNCTION */
+/*--------------------------------------------------------------------------*/
+
+int main(int argc, char* argv[]) {
+    cout << "--------------------------------" << endl;
+    cout << "         RSA Public Key" << endl;
+    cout << "--------------------------------" << endl;
+    
+
+    cout << "--------------------------------" << endl;
+    cout << "   El Gamal Digital Signature" << endl;
+    cout << "--------------------------------" << endl;
+}
